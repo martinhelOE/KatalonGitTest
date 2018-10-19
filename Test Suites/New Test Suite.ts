c85270e6c5f2453dc10322e81fe3bc3e2bbd26fe
martinhelOE <Trinity111!>
2018-10-19 17:26:23 -0300

    Test Suite Collection created

---
+++ Test Suites/New Test Suite.ts
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>New Test Suite</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>0be4f939-cf3f-475b-b535-8d48107a16fe</testSuiteGuid>
+   <testCaseLink>
+      <guid>6005f686-013e-45bb-b699-ee57433f623a</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Test 1 by Tom</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
